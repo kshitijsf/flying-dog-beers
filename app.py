f4e32854e47dd904c90c2bf95bf7ee2096bf201b
kshitijsf <85154555+kshitijsf@users.noreply.github.com>
2021-06-01 14:35:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
+import pandas as pd
+import dash_auth
+import dash_bootstrap_components as dbc
+import dash_table
+import plotly.express as px
+from dash.dependencies import Input, Output
 
 ########### Define your variables
 beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
@@ -37,27 +43,101 @@ beer_layout = go.Layout(
     title = mytitle
 )
 
-beer_fig = go.Figure(data=beer_data, layout=beer_layout)
+#beer_fig = go.Figure(data=beer_data, layout=beer_layout)
 
+customer = pd.read_csv('customer.csv')
+VALID_USERNAME_PASSWORD_PAIRS = {'hello': 'world'}
 
-########### Initiate the app
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+customer_options = [{'label': i, 'value': i} for i in customer['Customer Name'].unique()]
+
+raw_country_list = customer['Country Name'].unique()
+cleaned_country_list = [x for x in raw_country_list if str(x) != 'nan']
+country_options = [{'label': i, 'value': i} for i in cleaned_country_list]
+
+employee_options = [{'label': i, 'value': i} for i in customer['Employee '].unique()]
+
+data_options = [{'label':'Customer Name', 'value':'Customer Name'}, {'label':'Country', 'value':'Country'}, {'label':'Employee', 'value':'Employee'}]
+
+app = dash.Dash(external_stylesheets=[dbc.themes.SOLAR])
 server = app.server
-app.title=tabtitle
-
-########### Set up the layout
-app.layout = html.Div(children=[
-    html.H1(myheading),
-    dcc.Graph(
-        id='flyingdog',
-        figure=beer_fig
-    ),
-    html.A('Code on Github', href=githublink),
-    html.Br(),
-    html.A('Data Source', href=sourceurl),
-    ]
+auth = dash_auth.BasicAuth(
+    app,
+    VALID_USERNAME_PASSWORD_PAIRS
 )
+#print(cleaned_country_list)
+app.layout = html.Div([
+    html.H1('Customer Database for Traders'),
+    html.Label('Sing Fuels Proprietary Data', style={"fontSize": "20px"}),
+    #dcc.Dropdown(id='dropdown',options=[{'label': '', 'value': ''}], value='Search'),
+    dbc.Row([
+        dbc.Col(
+            [html.Label('Search by Customer Name:'),
+            dcc.Dropdown(id='customer-dropdown',
+            options=customer_options, placeholder='Customer Name'
+            )
+            ]),
+        dbc.Col(
+            [html.Label('Search by Country:'),
+            dcc.Dropdown(id='country-dropdown',
+            options=country_options, placeholder='Country'
+            )
+            ]),
+        # dbc.Col(
+        #     [html.Label('Search by Employee:'),
+        #     dcc.Dropdown(id='employee-dropdown',
+        #     options=employee_options, placeholder='Employee'
+        #     )
+        #     ])
+    ]),
+    dash_table.DataTable(id='customer-info',
+    columns=[{'name':col, 'id':col} for col in customer.columns])#,data=customer.to_dict('records'))
+])
+
+@app.callback(
+    Output('customer-info', 'data'),
+    Input('customer-dropdown', 'value'),
+    Input('country-dropdown', 'value'),
+    #Input('employee-dropdown', 'value')
+)
+
+def update_table_by_customer_name(selected_customer, selected_country):
+    print(selected_customer, selected_country)
+    filtered_customer = customer[customer['Customer Name'] == selected_customer]
+    if (selected_country is None):# & (selected_employee is None):
+        filtered_customer = customer[customer['Customer Name'] == selected_customer]
+    elif (selected_customer is None): #& (selected_employee is None):
+        filtered_customer = customer[customer['Country Name'] == selected_country]
+    # elif (selected_customer is None) & (selected_country is None):
+    #     filtered_customer = customer[customer['Employee '] == selected_employee]
+    elif (not(selected_customer is None)) & (not(selected_country is None)):# & (selected_employee is None):
+        filtered_customer = customer[(customer['Customer Name'] == selected_customer) & (customer['Country Name'] == selected_country)]
+    # elif (not(selected_customer is None)) & (selected_country is None) & (not(selected_employee is None)):
+    #     #print((not(selected_customer is None)) & (selected_country is None) & (not(selected_employee is None)))
+    #     filtered_customer = customer[(customer['Customer Name'] == selected_customer) & (customer['Employee '] == selected_employee)]
+    # elif (selected_customer is None) & (not(selected_country is None)) & (not(selected_employee is None)):
+    #     filtered_customer = customer[(customer['Country Name'] == selected_country) & (customer['Employee '] == selected_employee)]
+    # elif (not(selected_country is None)) & (not(selected_employee is None)) & (not(selected_customer is None)):
+    #     filtered_customer = customer[(customer['Country Name'] == selected_country) & (customer['Employee '] == selected_employee) & (customer['Customer Name'] == selected_customer)] 
+
+    return filtered_customer.to_dict('records')
+########### Initiate the app
+# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+# app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+# server = app.server
+# app.title=tabtitle
+
+# ########### Set up the layout
+# app.layout = html.Div(children=[
+#     html.H1(myheading),
+#     dcc.Graph(
+#         id='flyingdog',
+#         figure=beer_fig
+#     ),
+#     html.A('Code on Github', href=githublink),
+#     html.Br(),
+#     html.A('Data Source', href=sourceurl),
+#     ]
+# )
 
 if __name__ == '__main__':
     app.run_server()
